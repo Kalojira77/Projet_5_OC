1c430f00eaf6dc6c7dbc70881cf0757b716e6d3d
kalojira <kalojira77@gmail.com>
2025-05-29 20:23:26 +0200

    debug deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/logement/:id" element={<Logement />} />
-            {/* <Route path="*" element={<Error />} /> */}
+            <Route path="*" element={<Error />} />
           </Routes>
           </div>
         <Footer />
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ← C'est ça qui corrige le problème GitHub Pages
+  base: '/', 
   plugins: [react()],
   server: {
     watch: {

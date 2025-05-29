89ec1a5d4035ffcce1dec93ddb95c39301204f69
kalojira <kalojira77@gmail.com>
2025-05-29 20:30:27 +0200

    debug deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', 
+  base: '/Projet_5_OC/', 
   plugins: [react()],
   server: {
     watch: {
